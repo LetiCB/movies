5888add132f47c97f8feb5b08c34c79fbe4cb9a2
LetiCB <lcastrobrisson@gmail.com>
2023-05-14 00:23:17 -0300

    Update App.js index.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutPage from "./pages/About";
 import ContactPage from "./pages/Contact";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { useDispatch } from "react-redux";
+import { loadMoviesData } from "./redux/slices/movies/moviesSlice";
+import axios from "axios";
+
 
 
 function App() {
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    loadMovies();
+  }, []);
+
+  // --- Movies ---  //
+  function loadMovies() {
+    axios
+      .get("https://hp-api.onrender.com/api/characters")
+      .then((response) => {
+        dispatch(loadMoviesData(response.data));
+        // console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
