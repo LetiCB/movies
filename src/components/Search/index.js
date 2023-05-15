d83fe7bd35ef76370b2756addc260a5553e4da8a
LetiCB <lcastrobrisson@gmail.com>
2023-05-14 21:01:39 -0300

    Modify search component to enable live filtering

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,14 +3,13 @@ import "./styles/search.css";
 
 export default function Search({onSearch}){
     let [searchQuery, setSearchQuery] = useState("");
+
     let handleInputChange = (e) => {
       setSearchQuery(e.target.value);
+      onSearch(e.target.value);
     };
-    let handleSearch = () => {
-        onSearch(searchQuery);
-      };
 
-      return (
+    return (
         <>
           <div className="search-container">
             <div className="search-input-div">
@@ -23,19 +22,8 @@ export default function Search({onSearch}){
                 onChange={handleInputChange}
               />
             </div>
-            <div className="seach-button">
-              <input
-                type="submit"
-                className="btn"
-                value="search"
-                onClick={handleSearch}
-              />
-            </div>
+            <div className="searh-hp">Search</div>
         </div>
         </>
     )
 }
-
-
-
-
