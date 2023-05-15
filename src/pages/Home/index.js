7fa1fcb60e5f66d3a27c0c45183384d4981418fe
LetiCB <lcastrobrisson@gmail.com>
2023-05-14 22:06:44 -0300

    Update Home page to enable search by actor or character name

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterDataByActor } from "../../redux/slices/movies/moviesSlice";
+import { filterDataByActor, filterDataByCharacter } from "../../redux/slices/movies/moviesSlice";
 import { store } from '../../redux/store';
 import HarryPotterSpecial from "../../components/HarryPotter";
 import { fillHarryCard } from "../../utils/helpers";
@@ -10,33 +10,39 @@ import AboutHP from "../../components/AboutHP";
 import { Link } from 'react-router-dom';
 
 export default function Home() {
-
-    let movies = useSelector((store)=> store.movies.data)
-    // console.log(movies)
-    let filteredMovies = useSelector((store) => store.movies.filteredData);
-    let dispatch = useDispatch();
+    let movies = useSelector((store) => store.movies.data);
     let [searchQuery, setSearchQuery] = useState("");
-  
+    let dispatch = useDispatch();
+
     let handleSearch = (query) => {
         setSearchQuery(query);
         dispatch(filterDataByActor(query));
+        dispatch(filterDataByCharacter(query));
     };
 
-    let aboutHPData = useSelector((store)=> store.aboutHP.data);
-    console.log(aboutHPData);
+    let filteredMovies = movies.filter(
+        (movie) =>
+            movie.actor.toLowerCase().includes(searchQuery) ||
+            movie.name.toLowerCase().includes(searchQuery)
+    );
+
+    let displayCards = searchQuery === "" ? movies.slice(0, 25) : filteredMovies;
+
+    let aboutHPData = useSelector((store) => store.aboutHP.data);
 
     return(
         <>
-        
         <HarryPotterSpecial/>
         <AboutHP intro={aboutHPData.intro} />
-        <div className="read-more"><Link to={"/abouthp"}>Learn more about The Harry Potter Series</Link></div>
+        <div className="read-more">
+            <Link to={"/abouthp"}>Learn more about The Harry Potter Series</Link>
+        </div>
         <Search onSearch={handleSearch} />
         <div className="cards-container">
-            {searchQuery === "" ? (
-            movies.slice(0, 25).map(fillHarryCard)
+            {displayCards.length > 0 ? (
+                displayCards.map(fillHarryCard)
             ) : (
-            filteredMovies.slice(0, 25).map(fillHarryCard)
+                <p>Sorry, no results were found.</p>
             )}
         </div>
         </>
